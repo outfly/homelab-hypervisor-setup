c0c0e6db66c4243e2b6b77fd025c893ab13e5226
outfly <outfly.004@gmail.com>
2026-03-20 01:09:47 +0300

    Update iommu-groups.sh

--- scripts/iommu-groups.sh
+++ scripts/iommu-groups.sh
@@ -1,8 +1,7 @@
-#!/bin/env bash
+#!/bin/bash
 shopt -s nullglob
-for g in `find /sys/kernel/iommu_groups/* -maxdepth 0 -type d | sort -V`; do
-    echo
-    echo "IOMMU Group ${g##*/}:"
+for g in /sys/kernel/iommu_groups/*; do
+    echo -e "\nIOMMU Group ${g##*/}:"
     for d in $g/devices/*; do
         echo -e "\t$(lspci -nns ${d##*/})"
     done;
